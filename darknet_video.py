134c0ea1be42bc19d7193a3999b0a911d9ad8e1f
Buddy Lim <buddy.lim@live.com.my>
2019-12-08 23:23:31 +0800

    Baseline vehicle size branch

--- darknet_video.py
+++ darknet_video.py
@@ -7,12 +7,15 @@ import cv2
 import numpy as np
 import time
 import darknet
-import simpleaudio as sa 
+# import simpleaudio as sa 
+import csv
 
 #TODO: check FFMPEG to download FFMPEG static library
 #TODO: parse video & warning audio into output video
 #TODO: check resolution settings for the system
 #TODO: create a GUI to for users to input the video
+#TODO: Baseline average size of car 
+#TODO: Interpolate the resolution of 512x512 to 1080p & 720p
 
 def convertBack(x, y, w, h):
     xmin = int(round(x - (w / 2)))
@@ -23,30 +26,28 @@ def convertBack(x, y, w, h):
 
 
 def cvDrawBoxes(detections, img):
-    for detection in detections:
+    for counter,detection in enumerate(detections):
         x, y, w, h = detection[2][0],\
             detection[2][1],\
             detection[2][2],\
             detection[2][3]
-        # xmin, ymin, xmax, ymax = convertBack(
-        #     float(x), float(y), float(w), float(h))
-        # pt1 = (xmin, ymin)
-        # pt2 = (xmax, ymax)
-        # cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
-        # cv2.putText(img,
-        #             detection[0].decode() +
-        #             " [" + str(round(detection[1] * 100, 2)) + "]",
-        #             (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-        #             [0, 255, 0], 2)
-
-
-        if detection[0].decode() == "car":
-            print("Width: " + str(w) +"\n" + "Height: " + str(h))
-            if w > 140 and h > 80:
-                wave_obj = sa.WaveObject.from_wave_file("bleepsfxwav.wav")
-                subprocess.call(cmd,shell=True)
-                play_obj = wave_obj.play()
-                play_obj.wait_done()
+        xmin, ymin, xmax, ymax = convertBack(
+            float(x), float(y), float(w), float(h))
+        pt1 = (xmin, ymin)
+        pt2 = (xmax, ymax)
+        cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
+        cv2.putText(img,
+                    detection[0].decode() +
+                    " [" + str(counter) + " "+ str(round(detection[1] * 100, 2)) + "]",
+                    (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    [0, 255, 0], 2)
+        # if detection[0].decode() == "car":
+        #     print("Width: " + str(w) +"\n" + "Height: " + str(h))
+        #     if w > 140 and h > 80:
+        #         wave_obj = sa.WaveObject.from_wave_file("bleepsfxwav.wav")
+        #         subprocess.call(cmd,shell=True)
+        #         play_obj = wave_obj.play()
+        #         play_obj.wait_done()
     return img
 
 
@@ -59,8 +60,8 @@ cmd = 'ffmpeg -i input.mp4 -i music.mp3 -codec:v copy -codec:a aac -b:a 192k \
 -shortest output.mp4'
 
 def YOLO():
-
-    global metaMain, netMain, altNames
+    
+    global metaMain, netMain, altNames, detections
     configPath = "cfg\yolov3.cfg"
     weightPath = "../../../yolov3.weights"
     metaPath = "data\coco.data"
@@ -100,7 +101,7 @@ def YOLO():
         except Exception:
             pass
     #cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("CarCrashV1-edit.mp4")
+    cap = cv2.VideoCapture("C:Users\\user\\Pictures\\test.mp4")
     cap.set(3, cv2.CAP_PROP_FRAME_HEIGHT)
     cap.set(4, cv2.CAP_PROP_FRAME_WIDTH)
     out = cv2.VideoWriter(
@@ -112,12 +113,15 @@ def YOLO():
     # Create an image we reuse for each detect
     darknet_image = darknet.make_image(darknet.network_width(netMain),
                                     darknet.network_height(netMain),3)
-   
+    ###
+    upper_left = (50, 50)
+    bottom_right = (300, 300)
+    ###
     while True:
 
         prev_time = time.time()
         ret, frame_read = cap.read()
-        
+    
         #If end of video, break loop. In some IDEs like Spyder, without this code, 
         #the window frame will still persist and requires the user to force close via task manager
         if(np.shape(frame_read) == ()): 
@@ -129,13 +133,13 @@ def YOLO():
                                 (darknet.network_width(netMain),
                                     darknet.network_height(netMain)),
                                 interpolation=cv2.INTER_LINEAR)
-        # frame_resized = cv2.resize(frame_rgb,
-        #                         (1280,
-        #                            720),
-        #                         interpolation=cv2.INTER_LINEAR)
-
+        #TODO: Pause video so that we can get better car data
+        #Draws black borders on the sides so that YOLO only detects the mostly the middle section of the video
+        frame_resized = cv2.rectangle(frame_resized,(0,0),(90,416),(0,0,0),-1)
+        frame_resized = cv2.rectangle(frame_resized,(326,0),(416,416),(0,0,0),-1)
+        
         darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
-        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.7)
         image = cvDrawBoxes(detections, frame_resized)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         #print(1/(time.time()-prev_time))
