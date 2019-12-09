79d3491e53de429828bbecec13d925ad2526d847
Buddy Lim <buddy.lim@live.com.my>
2019-12-09 17:32:30 +0800

    Added pause functionality w/ framecount

--- darknet_video.py
+++ darknet_video.py
@@ -7,6 +7,7 @@ import cv2
 import numpy as np
 import time
 import darknet
+ 
 # import simpleaudio as sa 
 import csv
 
@@ -17,13 +18,14 @@ import csv
 #TODO: Baseline average size of car 
 #TODO: Interpolate the resolution of 512x512 to 1080p & 720p
 
+framecount = 0
+
 def convertBack(x, y, w, h):
     xmin = int(round(x - (w / 2)))
     xmax = int(round(x + (w / 2)))
     ymin = int(round(y - (h / 2)))
     ymax = int(round(y + (h / 2)))
     return xmin, ymin, xmax, ymax
-
 
 def cvDrawBoxes(detections, img):
     for counter,detection in enumerate(detections):
@@ -41,19 +43,28 @@ def cvDrawBoxes(detections, img):
                     " [" + str(counter) + " "+ str(round(detection[1] * 100, 2)) + "]",
                     (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                     [0, 255, 0], 2)
-        # if detection[0].decode() == "car":
-        #     print("Width: " + str(w) +"\n" + "Height: " + str(h))
-        #     if w > 140 and h > 80:
-        #         wave_obj = sa.WaveObject.from_wave_file("bleepsfxwav.wav")
-        #         subprocess.call(cmd,shell=True)
-        #         play_obj = wave_obj.play()
-        #         play_obj.wait_done()
+
+
+            # if detection[0].decode() == "car":
+            #     print("Width: " + str(w) +"\n" + "Height: " + str(h))
+            #     if w > 140 and h > 80:
+            #         wave_obj = sa.WaveObject.from_wave_file("bleepsfxwav.wav")
+            #         subprocess.call(cmd,shell=True)
+            #         play_obj = wave_obj.play()
+            #         play_obj.wait_done()
+    with open('output.csv', 'w+', newline='') as f_output:
+        csv_output = csv.writer(f_output,delimiter=',')
+        print("CSV Opened")
+        if(framecount % 5 == 0):    
+            csv_output.writerows(map(lambda x: [x],detection))
+
     return img
 
 
 netMain = None
 metaMain = None
 altNames = None
+
 #cmd = 'ffmpeg -i bleepsfxwav.wav -map 0:0 -map 0:1 -map 1:0 -map 2:0 -c:v copy -c:a copy output.avi'
 cmd = 'ffmpeg -i input.mp4 -i music.mp3 -codec:v copy -codec:a aac -b:a 192k \
 -strict experimental -filter_complex "amerge,pan=stereo:c0<c0+c2:c1<c1+c3" \
@@ -61,7 +72,8 @@ cmd = 'ffmpeg -i input.mp4 -i music.mp3 -codec:v copy -codec:a aac -b:a 192k \
 
 def YOLO():
     
-    global metaMain, netMain, altNames, detections
+    global metaMain, netMain, altNames, detections,framecount
+
     configPath = "cfg\yolov3.cfg"
     weightPath = "../../../yolov3.weights"
     metaPath = "data\coco.data"
@@ -101,7 +113,7 @@ def YOLO():
         except Exception:
             pass
     #cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("C:Users\\user\\Pictures\\test.mp4")
+    cap = cv2.VideoCapture("C:\\Users\\user\\Documents\\FYP\\RearEndAccident3-1080.mp4")
     cap.set(3, cv2.CAP_PROP_FRAME_HEIGHT)
     cap.set(4, cv2.CAP_PROP_FRAME_WIDTH)
     out = cv2.VideoWriter(
@@ -113,43 +125,54 @@ def YOLO():
     # Create an image we reuse for each detect
     darknet_image = darknet.make_image(darknet.network_width(netMain),
                                     darknet.network_height(netMain),3)
-    ###
-    upper_left = (50, 50)
-    bottom_right = (300, 300)
-    ###
+
     while True:
 
         prev_time = time.time()
         ret, frame_read = cap.read()
-    
+        framecount = framecount + 1
+
         #If end of video, break loop. In some IDEs like Spyder, without this code, 
         #the window frame will still persist and requires the user to force close via task manager
         if(np.shape(frame_read) == ()): 
-            print("Broke the loop")     
+            print("Broke the loop\n")
+            print("Framecount: " + str(framecount))     
             break 
-        
+
         frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
         frame_resized = cv2.resize(frame_rgb,
                                 (darknet.network_width(netMain),
                                     darknet.network_height(netMain)),
                                 interpolation=cv2.INTER_LINEAR)
-        #TODO: Pause video so that we can get better car data
+
         #Draws black borders on the sides so that YOLO only detects the mostly the middle section of the video
-        frame_resized = cv2.rectangle(frame_resized,(0,0),(90,416),(0,0,0),-1)
-        frame_resized = cv2.rectangle(frame_resized,(326,0),(416,416),(0,0,0),-1)
+        #(9th/12 Dec: Uncessary, better to just get all the object detected and just filter out the car with small areas)
+
+        # frame_resized = cv2.rectangle(frame_resized,(0,0),(90,416),(0,0,0),-1)
+        # frame_resized = cv2.rectangle(frame_resized,(326,0),(416,416),(0,0,0),-1)
         
         darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
         detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.7)
         image = cvDrawBoxes(detections, frame_resized)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #print(1/(time.time()-prev_time))
-        cv2.imshow('Warning-Sys', image)
         out.write(image)
+        cv2.imshow('Warning-Sys', image)
+
+        key = cv2.waitKey(1) & 0xff
 
         #Break the loop when Q is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'): 
+        if key == ord('q'): 
+            print('Broke the loop\n')
+            print("Framecount: " + str(framecount)) 
             break
-            print('Broke the loop')
+        
+        #Pauses the loop/video
+        if (key == ord('p')):
+            cv2.waitKey(-1)
+
+        #print(1/(time.time()-prev_time))
+        
+
     cap.release()
     out.release()
 
